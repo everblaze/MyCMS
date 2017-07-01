808db8f02548bc033bf8d8ba6e6ca90d8c709950
oliverbob <oliver.lagumen@facegod.us>
2017-06-30 22:17:56 -1200

    '

--- helpers/config.php
+++ helpers/config.php
@@ -3,7 +3,7 @@
  * @Author: Oliver Bob Lagumen
  * @Date:   2017-05-15 20:52:34
  * @Last Modified by:   Oliver Bob Lagumen
- * @Last Modified time: 2017-06-30 22:08:54
+ * @Last Modified time: 2017-06-30 22:17:33
  */
 
 $whitelist = array(
@@ -20,7 +20,7 @@ name = 'name_brokers_v2'
 */
 
 if(!in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
-	define("DB_HOST", "localhost'");
+	define("DB_HOST", "localhost");
 	define("DB_USER", "oliverbob_br");
 	define("DB_PASS", "m4U.Net.@64!");
 	define("DB_NAME", "oliverbob_br");
